a8bc0ba5645a90e9f62e75a61d8b6d77168f2825
Milad Raeisi <miladsoft@yahoo.com>
2024-11-04 21:13:18 +0400

    Update code

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'cryptocurrency-app'`, () => {
+  it(`should have as title 'kcc-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('cryptocurrency-app');
+    expect(app.title).toEqual('kcc-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, cryptocurrency-app');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, kcc-app');
   });
 });
